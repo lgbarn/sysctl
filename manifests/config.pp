35c419064de383306bc0d020176d9d9f8c2bb301
Luther Barnum <luther.barnum@gmail.com>
2016-01-06 08:58:40 -0500

    Changed permissions for serveral files and changed layout of module to conform to best practice.

---
+++ manifests/config.pp
@@ -0,0 +1,28 @@
+# Class: sysctl
+#
+# This module manages sysctl
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+class sysctl::config {
+  file { 'sysctl_conf':
+    ensure => 'file',
+    group  => '0',
+    mode   => '0644',
+    owner  => '0',
+    path   => '/etc/sysctl.conf',
+  }
+
+  exec { 'sysctl':
+    command     => 'sysctl -p',
+    refreshonly => true,
+    path        => '/sbin/',
+    subscribe   => File['sysctl_conf'],
+  }
+}
